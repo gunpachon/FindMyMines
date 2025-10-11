4678d8004a135afcad087ce6bd0fe562ae5d96fb
idealsh <141259309+idealsh@users.noreply.github.com>
2025-10-11 11:03:15 +0700

    fix: added font import to layout

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -2,6 +2,7 @@
   import "../app.css";
   import favicon from "$lib/assets/favicon.svg";
   import "@fontsource-variable/jaro";
+  import "@fontsource/jersey-25";
 
   let { children } = $props();
 </script>
