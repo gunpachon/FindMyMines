5fb6e1c4b80901d950f3965a369e82f0fb57762b
idealsh <141259309+idealsh@users.noreply.github.com>
2025-10-14 13:29:59 +0700

    feat: change font loading to block

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
   import "../app.css";
   import favicon from "$lib/assets/favicon.svg";
-  import "@fontsource-variable/jaro";
-  import "@fontsource/jersey-25";
 
   let { children } = $props();
 </script>
