a47bbcfead91e8fe6c03819b7a91821f86b7c6a0
Pacharah <gunworkk@gmail.com>
2025-11-03 23:13:42 +0700

    feat: add reaction component

---
+++ apps/web/src/lib/components/Reaction.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { twMerge } from "../twMerge";
+  
+  import reactionCelebrate from "../assets/reaction-celebrate.svg";
+  import reactionFire from "../assets/reaction-fire.svg";
+  import reactionHeart from "../assets/reaction-heart.svg";
+
+  interface Props {
+    type: "celebrate" | "fire" | "heart";
+    disabled?: boolean;
+    class?: string;
+    onclick?: () => void;
+  }
+
+  let {
+    type,
+    disabled = false,
+    class: className = "",
+    onclick,
+    ...restProps
+  }: Props = $props();
+
+  const reactionIcons = {
+    celebrate: reactionCelebrate,
+    fire: reactionFire,
+    heart: reactionHeart
+  };
+
+  const baseClasses ="w-20 h-16 p-2 rounded-xl inline-flex justify-center items-center transition hover:brightness-110 hover:scale-105 active:scale-95 disabled:cursor-not-allowed disabled:opacity-50";
+
+  const classes = twMerge(baseClasses, className);
+</script>
+
+<button type="button" {disabled} class={classes} {onclick} {...restProps}>
+  <img 
+    src={reactionIcons[type]} 
+    alt={`${type} reaction`}
+    class="w-20 h-20 object-contain"
+  />
+</button>
