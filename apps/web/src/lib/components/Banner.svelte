a9980d494f9bf60c9d8371bca06c9c009d011dd6
idealsh <141259309+idealsh@users.noreply.github.com>
2025-11-07 09:28:55 +0700

    fix: hotfix dark mode

--- apps/web/src/lib/components/Banner.svelte
+++ apps/web/src/lib/components/Banner.svelte
@@ -13,15 +13,15 @@
 
 <div
   class={twMerge(
-    "mx-auto flex flex-col items-center gap-7 rounded-4xl border border-light-gray bg-white/80 p-12 shadow-md",
+    "rounded-4xl border-light-gray text-be-mine-gray mx-auto flex flex-col items-center gap-7 border bg-white/80 p-12 shadow-md",
     classes,
   )}
 >
   {#if topText}
-    <p class="text-4xl text-be-mine-gray">{topText}</p>
+    <p class="text-4xl">{topText}</p>
   {/if}
   <img src={iconSource} alt="" srcset="" />
   {#if bottomText}
-    <p class="text-4xl text-be-mine-gray">{bottomText}</p>
+    <p class="text-4xl">{bottomText}</p>
   {/if}
 </div>
